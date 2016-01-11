8f23416cb65852709f1d171fcc7be4cafdba1a3d
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-11 01:30:50 -0500

    default to Rails.application.config.i18n.available_locales

--- lib/solidus_globalize/configuration.rb
+++ lib/solidus_globalize/configuration.rb
@@ -7,6 +7,6 @@ module SolidusGlobalize
     #
     # e.g. If available_locales are [:en, :es] admin can translate model records
     # to spanish as well. Once it's done :es can be added to supported_locales
-    preference :supported_locales, :array, default: [:en]
+    preference :supported_locales, :array, default: Rails.application.config.i18n.available_locales
   end
 end
